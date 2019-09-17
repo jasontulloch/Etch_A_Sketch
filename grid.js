7b2f46a0d75b6fd69473d585548ea52d5097c8fa
Jason Tulloch <jasonbtulloch@gmail.com>
2019-09-17 06:09:49 -0400

    Dynamic EAS that will change dimensions based on inputs and reset

--- grid.js
+++ grid.js
@@ -1,6 +1,9 @@
 // Build a 16x16 grid
 // Note: adding 'cell.innerText = (c + 1);' below 'let cell = document.createElement('div');' numbers the cells
 
+let rows = 16;
+let cols = 16;
+
 const container = document.getElementById('container');
 
 function makeGrid(rows, cols) {
@@ -20,16 +23,18 @@ function makeGrid(rows, cols) {
     };
 };
 
-makeGrid(16, 16);
+makeGrid(rows, cols);
 
 let btn = document.querySelector('#btn');
 btn.addEventListener('click', resetGrid);
 
 function resetGrid() {
     document.getElementById('container').innerHTML = "";
-    container.style.setProperty('--grid-rows', 16);
-    container.style.setProperty('--grid-cols', 16);
-    for (c = 0; c < (16 * 16); c++) {
+    let rows1 = prompt("Please select the width of the grid");
+    let cols1 = prompt("Please select the height of the grid");
+    container.style.setProperty('--grid-rows', rows1);
+    container.style.setProperty('--grid-cols', cols1);
+    for (c = 0; c < (rows1 * cols1); c++) {
         let cell = document.createElement('div');
         container.appendChild(cell).className = 'grid-item';
         cell.addEventListener('mouseenter', e => {
@@ -42,3 +47,7 @@ function resetGrid() {
         });
     };
 };
+
+
+//let rows1 = 5;
+//let cols1 = 5;
